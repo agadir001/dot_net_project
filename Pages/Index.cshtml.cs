8c0f87439a8bc4b7d4963cbe675736e7040b080d
agadir001 <110976654+agadir001@users.noreply.github.com>
2023-03-12 16:16:38 +0100

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+// a comment 
 
 namespace AspNetCoreDemo.Pages
 {
